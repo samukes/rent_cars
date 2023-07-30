db019ec5923da5d2cc6ff06a62c250bed2e8c747
Samuel Fontes <darkdarumia@gmail.com>
2023-07-30 20:47:52 -0300

    format

--- lib/rent_cars_web/components/core_components.ex
+++ lib/rent_cars_web/components/core_components.ex
@@ -302,8 +302,7 @@ defmodule RentCarsWeb.CoreComponents do
   end
 
   def input(%{type: "checkbox", value: value} = assigns) do
-    assigns =
-      assign_new(assigns, :checked, fn -> Form.normalize_value("checkbox", value) end)
+    assigns = assign_new(assigns, :checked, fn -> Form.normalize_value("checkbox", value) end)
 
     ~H"""
     <div phx-feedback-for={@name}>
