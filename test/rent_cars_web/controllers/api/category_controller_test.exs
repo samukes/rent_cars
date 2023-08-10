2ed35d8931459a1de8d55d6787230d7983a5bf13
Samuel Fontes <darkdarumia@gmail.com>
2023-08-10 01:11:25 -0300

    feat: tdd path-way

---
+++ test/rent_cars_web/controllers/api/category_controller_test.exs
@@ -0,0 +1,19 @@
+defmodule RentCarsWeb.Api.CategoryControllerTest do
+  @moduledoc false
+
+  use RentCarsWeb.ConnCase
+
+  describe "GET index" do
+    test "should list all categories", %{conn: conn} do
+      result = get(conn, "/api/categories")
+
+      assert json_response(result, 200)["data"] == [
+               %{
+                 "description" => "pumpkin 1223",
+                 "id" => "123123",
+                 "name" => "SPOT"
+               }
+             ]
+    end
+  end
+end
