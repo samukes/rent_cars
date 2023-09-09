458b8c2764438895adaf817fc495c8d9eb2e989e
Samuel Fontes <darkdarumia@gmail.com>
2023-09-09 17:52:15 -0300

    feat: add genrt specification apis

---
+++ lib/rent_cars_web/controllers/fallback_controller.ex
@@ -0,0 +1,24 @@
+defmodule RentCarsWeb.FallbackController do
+  @moduledoc """
+  Translates controller action results into valid `Plug.Conn` responses.
+
+  See `Phoenix.Controller.action_fallback/1` for more details.
+  """
+  use RentCarsWeb, :controller
+
+  # This clause handles errors returned by Ecto's insert/update/delete.
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(json: RentCarsWeb.ChangesetJSON)
+    |> render(:error, changeset: changeset)
+  end
+
+  # This clause is an example of how to handle resources that cannot be found.
+  def call(conn, {:error, :not_found}) do
+    conn
+    |> put_status(:not_found)
+    |> put_view(html: RentCarsWeb.ErrorHTML, json: RentCarsWeb.ErrorJSON)
+    |> render(:"404")
+  end
+end
